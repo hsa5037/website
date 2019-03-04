0ae659190033260d6ef2f059f04fcd71212506bd
Humza Ahmed <hsa5037@gmail.com>
2019-03-03 22:18:29 -0500

    Added minor javascript features

---
+++ Project/js/script.js
@@ -0,0 +1,17 @@
+document.getElementById("aboutMe").addEventListener("mouseover", function(event) {
+	event.target.style.color = "red";
+}, false);
+
+document.getElementById("aboutMe").addEventListener("mouseout", function(event) {
+	event.target.style.color = "";
+}, false);
+
+document.getElementById("projects").addEventListener("mouseover", function(event) {
+	event.target.style.color = "red";
+}, false);
+
+document.getElementById("projects").addEventListener("mouseout", function(event) {
+	event.target.style.color = "";
+}, false);
+
+
